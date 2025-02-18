c6ba7d9287e32431401ed3a0c27a598420c5d718
Sai Yaswanth <saiyaswanth@192.168.1.5>
2025-02-18 19:23:29 +0530

    new

--- myapp/src/Components/navbars.jsx
+++ myapp/src/Components/navbars.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/NavbarComponent.css";
+import "../styles/NavbarComponent.css"; // Custom styles
 
 function NavbarComponent() {
   return (
     <Navbar className="navbar-custom" expand="lg">
       <Container>
-        <Navbar.Brand className="brand-text">React App</Navbar.Brand>
-        <Nav className="ms-auto">
-          <Nav.Link as={Link} to="/" className="nav-link-custom">Home</Nav.Link>
-          <Nav.Link as={Link} to="/compliance" className="nav-link-custom">Compliance</Nav.Link>
-          <Nav.Link as={Link} to="/eligibility" className="nav-link-custom">Eligibility</Nav.Link>
-          <Nav.Link as={Link} to="/resource" className="nav-link-custom">Resource</Nav.Link>
-          <Nav.Link as={Link} to="/reminder" className="nav-link-custom">Reminder</Nav.Link>
-          <Nav.Link as={Link} to="/contact" className="nav-link-custom">Contact</Nav.Link>
-        </Nav>
+        <Navbar.Brand as={Link} to="/" className="brand-text">
+          React App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" className="nav-link-custom">Home</Nav.Link>
+            <Nav.Link as={Link} to="/compliance" className="nav-link-custom">Compliance</Nav.Link>
+            <Nav.Link as={Link} to="/eligibility" className="nav-link-custom">Eligibility</Nav.Link>
+            <Nav.Link as={Link} to="/resource" className="nav-link-custom">Resource</Nav.Link>
+            <Nav.Link as={Link} to="/reminder" className="nav-link-custom">Reminder</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="nav-link-custom">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
